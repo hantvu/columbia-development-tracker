f0a38a66170cc3634ebef3951ade21b73abc03a7
Han Vu <88125474+HanVietnam@users.noreply.github.com>
2022-09-06 22:08:50 -0500

    Customized the files needed

---
+++ toolkit.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    siteTitle: `Columbia Development Tracker`,
+    siteUrl: `https://columbia-development-tracker.netlify.app`,
+    organization: `Columbia Missourian`,
+    city: `Columbia`,
+    state: `Missouri`,
+    boundingBox: [-92.43383208,38.8685322439,-92.2336998511,39.02940195],
+    primary: '#F1B82D',
+    secondary: '#FFF89A',
+    tertiary: '#000000',
+  }
